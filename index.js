241783d66bfb1ca012f8aaae548979cc62b2b27d
88-mahmoudmostafa <homo04020@gmail.com>
2026-07-09 12:22:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+ let hero= document.querySelector('.img-sc');
+ let cont= document.querySelector('.container');
+  let  h1= document.querySelector('h1');
+  let  span= document.querySelector('span');
+  let p =document.querySelector('p');
+
+ function heandleclick(phone,color,h,price,p){
+
+    hero.src=phone
+     cont.style.background=color
+     h1.innerHTML=h
+      span.innerHTML=price
+       there.innerHTML=p
+ }
+  
